4f0d1831153a5e1bad383e45bf8552d229e61b9c
Isabelle <loariyafiaba@gmail.com>
2023-06-11 07:44:20 +0100

    Added a .gitignore file + small changes to JSON file

--- src/cli.js
+++ src/cli.js
@@ -1,1 +1,6 @@
+// Create a main command for the CLI tool
+const { program } = require("commander");
+program.command("cli-mate");
  
+// Define subcommands for different functionalities
+program.command("fetch");
